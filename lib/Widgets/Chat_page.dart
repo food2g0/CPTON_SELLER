1e0919267c79355b063fbe7119bb6f3e80129028
food2g0 <foodtogo763@gmail.com>
2024-03-15 14:52:47 +0800

    final commit and push

--- lib/Widgets/Chat_page.dart
+++ lib/Widgets/Chat_page.dart
@@ -1,11 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 import 'package:cpton_food2go_sellers/Widgets/text_field.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../Components/chat_bubble.dart';
 import '../colors.dart';
 import '../services/chat_services.dart';
@@ -25,16 +27,29 @@ class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
   final ChatService _chatService = ChatService();
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final ImagePicker _imagePicker = ImagePicker();
 
   void sendMessage () async{
     if (_messageController.text.isNotEmpty){
       await _chatService.sendMessage(widget.receiverUserID, _messageController.text);
-
-      // clear controller
       _messageController.clear();
     }
   }
 
+  Future<void> sendImage() async {
+    final XFile? imageFile = await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
+      // Upload the image to Firebase Storage
+      Reference ref = FirebaseStorage.instance.ref().child('chat_images').child(DateTime.now().toString());
+      UploadTask uploadTask = ref.putFile(File(imageFile.path));
+
+      // Get the download URL once the image is uploaded
+      String imageUrl = await (await uploadTask).ref.getDownloadURL();
+
+      // Send the image URL along with the message
+      await _chatService.sendMessage(widget.receiverUserID, imageUrl);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,23 +57,24 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         backgroundColor: AppColors().red,
         title: Text(widget.receiverUserEmail,
-        style: TextStyle(
-          color: AppColors().white,
-          fontFamily: "Poppins",
-          fontSize: 12.sp
-        ),),
+          style: TextStyle(
+              color: AppColors().white,
+              fontFamily: "Poppins",
+              fontSize: 12.sp
+          ),),
       ),
       body: Column(
         children: [
-          Expanded(child: _buildMessageList(),
+          Expanded(
+            child: _buildMessageList(),
           ),
           _builMessageInput(),
-
           SizedBox(height: 20.sp,)
         ],
       ),
     );
   }
+
   // build message list
   Widget _buildMessageList(){
     return StreamBuilder(
@@ -73,13 +89,12 @@ class _ChatPageState extends State<ChatPage> {
           return ListView(
             children: snapshot.data!.docs.map((document) => _buildMessageItems(document)).toList(),
           );
-
         });
   }
 
-  //build message item
+  // build message item
   Widget _buildMessageItems(DocumentSnapshot document){
-    Map<String, dynamic> data = document.data() as Map<String, dynamic >;
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
     var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
         ? Alignment.centerRight
         : Alignment.centerLeft;
@@ -88,40 +103,44 @@ class _ChatPageState extends State<ChatPage> {
       child: Padding(
         padding: EdgeInsets.all(8.0.w),
         child: Column(
-          crossAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid) ? CrossAxisAlignment.end
-              : CrossAxisAlignment.start,
-
-          mainAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid) ? MainAxisAlignment.end
-              : MainAxisAlignment.start,
+          crossAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid) ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          mainAxisAlignment: (data['senderId'] == _firebaseAuth.currentUser!.uid) ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
-            Text(data['senderEmail'],style:
-              TextStyle(fontFamily: "Poppins",
-              fontSize: 12.sp),),
-            SizedBox(height: 5.sp,),
-            ChatBubble(message: data['message']),
+            Text(data['senderEmail'], style: TextStyle(fontFamily: "Poppins", fontSize: 12.sp),),
+            SizedBox(height: 5.sp),
+            if (data['message'] != null && data['message'].toString().startsWith('http')) // Check if message is a URL (image)
+              Image.network(data['message'], width: 150, height: 150),
+            if (data['message'] != null && !data['message'].toString().startsWith('http')) // Display text message if not an image URL
+              ChatBubble(message: data['message']),
           ],
         ),
       ),
     );
   }
 
-
   // build message input
   Widget _builMessageInput(){
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 25.w),
       child: Row(
-        //text Field
         children: [
-          Expanded(child:
-          MyTextField(
-            controller: _messageController,
-            hint: 'Enter your Message',
+          IconButton(
+            onPressed: sendImage,
+            icon: Icon(Icons.image, size: 25.sp,),
           ),
+          Expanded(
+            child: MyTextField(
+              controller: _messageController,
+              hint: 'Enter your Message',
+            ),
           ),
-          IconButton(onPressed: sendMessage,
-              icon: Icon
-                (Icons.send,size: 25.sp,))
+
+
+          IconButton(
+            onPressed: sendMessage,
+            icon: Icon(Icons.send_rounded, size: 25.sp,
+            color: AppColors().red,),
+          ),
         ],
       ),
     );
--- lib/mainScreen/Earnings_screen.dart
+++ lib/mainScreen/Earnings_screen.dart
@@ -14,6 +14,7 @@ class EarningsScreen extends StatefulWidget {
 class _EarningsScreenState extends State<EarningsScreen> {
   late User? _user;
   double totalEarnings = 0.0;
+  double totalEarningsCashonDelivery = 0.0;
 
   @override
   void initState() {
@@ -28,20 +29,20 @@ class _EarningsScreenState extends State<EarningsScreen> {
       DocumentSnapshot<Map<String, dynamic>> salesSnapshot = await FirebaseFirestore.instance
           .collection('sellers')
           .doc(_user!.uid)
-          .collection('sales')
-          .doc('03_March')
           .get();
 
       // Check if the document exists
       if (salesSnapshot.exists) {
         // Extract the sale value from the document and update the total earnings
         setState(() {
-          totalEarnings = salesSnapshot['saleVal'] ?? 0.0;
+          totalEarnings = salesSnapshot['earningsGCash'] ?? 0.0;
+          totalEarningsCashonDelivery = salesSnapshot['earningsCashOnDelivery'] ?? 0.0;
         });
       } else {
         // If the document doesn't exist, set earnings to 0
         setState(() {
           totalEarnings = 0.0;
+          totalEarningsCashonDelivery = 0.0;
         });
       }
     } catch (error) {
@@ -52,11 +53,17 @@ class _EarningsScreenState extends State<EarningsScreen> {
   @override
   Widget build(BuildContext context) {
     // Calculate 10% of total earnings
-    double tenPercent = totalEarnings * 0.1;
+    double totalEarningsGcashAndCod = totalEarnings + totalEarningsCashonDelivery;
+    double tenPercent = (totalEarnings + totalEarningsCashonDelivery) * 0.1;
     // Calculate earnings after deducting 10%
-    double earningsAfterDeduction = totalEarnings - tenPercent;
+    double earningsAfterDeduction = totalEarnings  - tenPercent;
+
+    double turnOverAmount = totalEarningsCashonDelivery * 0.1;
+    String formattedTurnOverAmount = turnOverAmount.toStringAsFixed(2);
+    String formattedWithdraw = earningsAfterDeduction.toStringAsFixed(2);
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.red,
@@ -78,6 +85,82 @@ class _EarningsScreenState extends State<EarningsScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
+                SizedBox(
+                  width: 180.w,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors().green.withOpacity(0.5), // Set the opacity here
+                      borderRadius: BorderRadius.circular(10),
+
+                    ),
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'G-cash',
+                          style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                        ),
+                        SizedBox(height: 8.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'images/peso.png', // Path to your image asset
+                          width: 12.w,
+                          height: 12.h,
+                        ),
+                        SizedBox(width: 5.w),
+                        Text(
+                          '$totalEarnings',
+                          style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                        ),
+                      ],
+                      )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20), // Add spacing between the first and second container
+                SizedBox(
+                  width: 180.w,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors().green.withOpacity(0.5), // Set the opacity here
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Cash on Delivery',
+                          style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                        ),
+                        SizedBox(height: 8.h),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'images/peso.png', // Path to your image asset
+                              width: 12.w,
+                              height: 12.h,
+                            ),
+                            SizedBox(width: 5.w),
+                            Text(
+                              '$totalEarningsCashonDelivery',
+                              style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Add spacing between the first row and the second container
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
                 Container(
                   decoration: BoxDecoration(
                     color: AppColors().green.withOpacity(0.5), // Set the opacity here
@@ -91,10 +174,53 @@ class _EarningsScreenState extends State<EarningsScreen> {
                         style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
                       ),
                       SizedBox(height: 8.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'images/peso.png', // Path to your image asset
+                            width: 12.w,
+                            height: 12.h,
+                          ),
+                          SizedBox(width: 5.w),
+                          Text(
+                            '$totalEarningsGcashAndCod',
+                            style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColors().green.withOpacity(0.5), // Set the opacity here
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
                       Text(
-                        '\Php $totalEarnings',
+                        'Withdraw',
                         style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
                       ),
+                      SizedBox(height: 8.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'images/peso.png', // Path to your image asset
+                            width: 12.w,
+                            height: 12.h,
+                          ),
+                          SizedBox(width: 5.w),
+                          Text(
+                            '$formattedWithdraw',
+                            style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
@@ -107,34 +233,25 @@ class _EarningsScreenState extends State<EarningsScreen> {
                   child: Column(
                     children: [
                       Text(
-                        'Deduction (10%)',
-                        style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                        'Amount to turnover',
+                        style: TextStyle(fontSize: 8.sp, fontFamily: "Poppins"),
                       ),
                       SizedBox(height: 8.h),
-                      Text(
-                        '\Php $earningsAfterDeduction',
-                        style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColors().green.withOpacity(0.5), // Set the opacity here
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        '10% Deduction',
-                        style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        '\Php $tenPercent',
-                        style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
-                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'images/peso.png', // Path to your image asset
+                            width: 12.w,
+                            height: 12.h,
+                          ),
+                          SizedBox(width: 5.w),
+                          Text(
+                            '$formattedTurnOverAmount',
+                            style: TextStyle(fontSize: 10.sp, fontFamily: "Poppins"),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
@@ -145,8 +262,8 @@ class _EarningsScreenState extends State<EarningsScreen> {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 decoration: BoxDecoration(
-                  color: AppColors().white1,
-                  borderRadius: BorderRadius.circular(10)
+                    color: AppColors().white1,
+                    borderRadius: BorderRadius.circular(10)
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -166,6 +283,130 @@ class _EarningsScreenState extends State<EarningsScreen> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+
+            ElevatedButton(onPressed: (){
+              requestTurnover();
+            },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors().red,
+                    minimumSize: Size(150, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                    )
+                ),
+                child: Text("Turn Over", style: TextStyle(
+                    color: AppColors().white,
+                    fontFamily: "Poppins",
+                    fontSize: 12.sp,
+
+                ),))
+          ],
+        ),
+      ),
     );
   }
+  void requestTurnover() {
+    TextEditingController referenceController = TextEditingController(); // Controller for reference number
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          physics: BouncingScrollPhysics(), // Allow the sheet to adjust its height according to its content
+          reverse: true, // Scroll to the bottom of the sheet initially
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom, // Ensure the bottom padding adjusts for the keyboard
+            ),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: referenceController, // Assign controller to reference number text field
+                    decoration: InputDecoration(labelText: 'Reference Number'),
+                    maxLength: 13,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        'Gcash Number: ', // Display the Gcash number as text
+                        style: TextStyle(
+                          fontSize: 10.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '09271679585 - Paolo Somido', // Replace 'Your Gcash Number Here' with the actual Gcash number
+                        style: TextStyle(
+                          fontSize: 10.sp,
+                          fontFamily: "Poppins"
+                          // Add your styles here
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Save turnover details to Firestore with reference number
+                      saveTurnoverRequest(referenceController.text);
+                      Navigator.pop(context); // Close the modal
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors().red,
+                      minimumSize: Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text('Submit',
+                    style: TextStyle(color: AppColors().white,
+                    fontFamily: "Poppins"),),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+
+  void saveTurnoverRequest(String referenceNumber) {
+    // Reset the value of totalEarningsCashonDelivery * 0.1 to 0
+    double turnOverAmount = totalEarningsCashonDelivery * 0.1;
+
+    // Save turnover details to Firestore
+    FirebaseFirestore.instance.collection('turnover').doc(_user!.uid).set({
+      'userId': _user!.uid,
+      'status': 'sent',
+      'amount': turnOverAmount, // Reset to 0
+      'referenceNumber': referenceNumber,
+      'timestamp': DateTime.now(),
+    }).then((value) {
+      // Reset the value of earningsCashOnDelivery to 0 in the database
+      FirebaseFirestore.instance.collection('sellers').doc(_user!.uid).update({
+        'earningsCashOnDelivery': 0.0,
+      }).then((_) {
+        // Do something after updating the earningsCashOnDelivery field
+      }).catchError((error) {
+        print('Error updating earningsCashOnDelivery: $error');
+      });
+    }).catchError((error) {
+      print('Error saving turnover request: $error');
+    });
+  }
+
+
+
+
 }
+
